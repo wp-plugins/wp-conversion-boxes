d915f111995e0bb7b0cae7d938efec0173d8563c
ramshengale <ramshengale@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2015-05-15 09:33:51 +0000

    v2.5.1
    git-svn-id: https://plugins.svn.wordpress.org/wp-conversion-boxes/trunk@1161036 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- admin/includes/upgrade-message.php
+++ admin/includes/upgrade-message.php
@@ -1,5 +1,5 @@
 <?php
 
-$upgrade_message = "<span style='display: block; font-size: 12px; color: grey;'><a href='http://wpconversionboxes.com/?utm_source=Upgrade+To+Pro&utm_medium=link&utm_campaign=BuySellAds' target='_blank'><em>".__('Upgrade To Pro','wp-conversion-boxes')."</em></a> ".__('to be able to use this feature.','wp-conversion-boxes')."</span>";
+$upgrade_message = "<span style='display: block; font-size: 12px; color: grey;'><a href='http://wpconversionboxes.com/?utm_source=Upgrade+To+Pro&utm_medium=link&utm_campaign=WPCB' target='_blank'><em>".__('Upgrade To Pro','wp-conversion-boxes')."</em></a> ".__('to be able to use this feature.','wp-conversion-boxes')."</span>";
 
 ?>
